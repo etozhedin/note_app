d6ec79d527c79e0b8afa57ac5a3b480b36007ccc
etozhedin <spsq0202@gmail.com>
2023-07-17 21:18:06 +0900

    proc change

--- backend/note_app/settings.py
+++ backend/note_app/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6zpvd&($jlp0p@navj*h$8ta^-ks&qqgo_5h$nu&936sk$eay_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['herokuappname.herokuapp.com']
 
 # Application definition
 
