dd91729ba075c918846fa2db78b189fb426c935d
etozhedin <spsq0202@gmail.com>
2023-07-23 19:26:19 +0900

    permission_classes for registration

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class UserRegistration(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [~IsAuthenticated]
