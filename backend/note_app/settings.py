b8f9ec0fd480119966e0bfe91239fb00a7b2dc52
etozhedin <spsq0202@gmail.com>
2023-08-03 23:06:15 +0900

    corsheaders added

--- backend/note_app/settings.py
+++ backend/note_app/settings.py
@@ -51,25 +51,25 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = []
-CORS_URL_REGEX = r"^/api/.*"
+# CORS_URL_REGEX = r"^/api/.*" только пути соответствующие этому регеху подверг. корс
 
-CORS_ALLOWED_ORIGINS =[]
-if DEBUG: 
-    CORS_ALLOWED_ORIGINS += [
-    'http://localhost:8111',
-    'https://localhost:8111',
-]
+# CORS_ALLOWED_ORIGINS =[]
+# if DEBUG: 
+#     CORS_ALLOWED_ORIGINS += [
+#     'http://localhost:8111',
+#     'https://localhost:8111',
+# ]
 
 ROOT_URLCONF = 'note_app.urls'
 
