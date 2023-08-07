701a50d381a4e1982c292da6bfddf431f4366418
etozhedin <spsq0202@gmail.com>
2023-08-08 02:02:25 +0900

    changed content limit

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=100)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
     created = models.DateTimeField(auto_now_add=True)
     updated =  models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, default=None)
