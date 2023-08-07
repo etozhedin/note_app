3900643e15695ac8978d63836061393b3c5166b8
etozhedin <spsq0202@gmail.com>
2023-08-08 02:05:06 +0900

    changes

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,5 +4,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    # Здесь вы можете добавить дополнительные поля для пользователя, если необходимо
-    pass
+    avatar_url = models.URLField(max_length = 200)
