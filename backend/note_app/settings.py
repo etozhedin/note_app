4efdfd743de3a2a363b809b784d6cf7d96f11950
etozhedin <spsq0202@gmail.com>
2023-08-08 00:34:27 +0900

    firebase keypath added

--- backend/note_app/settings.py
+++ backend/note_app/settings.py
@@ -13,11 +13,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import firebase_admin
+from firebase_admin import credentials, storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+FIREBASE_KEY_PATH = os.path.join(BASE_DIR.parent, 'serviceAccountKey.json')
+cred = credentials.Certificate(FIREBASE_KEY_PATH)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'note-app-b35bf.appspot.com'
+})
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
